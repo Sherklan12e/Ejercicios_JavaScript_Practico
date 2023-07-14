02b2f85c07093b4946bce7d3c6577ca520a37406
Sherklan12e <davismendoza.08@gmail.com>
2023-07-14 19:33:07 -0300

    seconds

--- ejercicio11/script.js
+++ ejercicio11/script.js
@@ -1,1 +1,11 @@
-let name = "name"
+//Escribe un programa que pida una frase y escriba cuántas de las letras que tiene son vocales
+
+let frase = prompt("Escriba la frase: ")
+var contador = 0
+var vocales = ["a","e","i","o","u"]
+
+for (var i = 0; i < frase.length; i++){
+    var letra = frase[i].toLowerCase()
+    if 
+
+}
