a1f7832b782b813c109ef486f7659bc454dea487
Sherklan12e <davismendoza.08@gmail.com>
2024-01-13 06:05:14 -0300

    changes

---
+++ Proyectos/Sheklan12e/Todo/script.js
@@ -0,0 +1,10 @@
+let Tareas = [];
+
+function agregarTareas(){// este el del buttom
+    const tareaEntrada = document.getElementById('tarea');// id del input
+    const tareaLista = document.getElementById('listaTarea')// id de la <ul>
+
+    
+
+
+}
