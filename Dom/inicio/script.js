a44f37544d312492561ef06f6337d9c70d5134ac
Sherklan12e <davismendoza.08@gmail.com>
2024-01-18 06:38:14 -0300

    Dom

--- Dom/inicio/script.js
+++ Dom/inicio/script.js
@@ -1,8 +1,52 @@
 const contenedor = document.getElementById('contenedor');
 const titulo = document.getElementById('titulo')
-console.log(contenedor.innerHTML)
+const toppings = document.getElementsByClassName('topping')
+const mytoppings = document.getElementsByTagName('li')
+// QUERY SELECTOR 
+const aceituna = document.querySelector('#aceituna');
+const aceitunas = document.querySelector('.topping.fondo-naranja');
+// queryselecto('ul li.fondo-naranja');
+// queryselecto('ul li:not(.fondo-marron)'); clase li pero que no tengan la clase fondo marron
 
-console.log(titulo.tagName)
+// queryselectall
+const enlace = document.getElementsByTagName('a')
+const toppingsl = document.querySelectorAll('.topping.fondo-naranja');
+const listatop = document.getElementById('lista-toppings')
+const primerTopping = document.querySelector('.topping')
 
 
+// console.log(contenedor.innerHTML)
+// console.log(toppings[2].textContent)
+// console.log(titulo.textContent)
 
+// console.log(mytoppings)
+
+// console.log(aceituna)   
+// console.log(aceitunas.innerText)
+
+// console.log(toppingsl)
+
+
+// Styles 
+// primerTopping.style.backgroundColor = 'blue';
+// primerTopping.style.color = '#6dff00';
+// primerTopping.style.textTransform = 'uppercase'
+
+
+// console.log(primerTopping.style)
+// console.log(listatop.innerText)
+// console.log(listatop.textContent)
+// console.log(listatop.innerHTML)
+
+// titulo.innerText = 'hola'
+
+//removeAtribute - elimina
+// setAtribute - actualiza sfd, asdfasd
+
+
+// console.log(enlace[0].setAttribute('href', 'https://facebook.com'));
+
+primerTopping.classList.add('mi');
+console.log(primerTopping.classList.contains('mi')) // Devuelve tru o false 
+// classList.remove('') y nombre de la clase
+console.log(primerTopping.innerText)
