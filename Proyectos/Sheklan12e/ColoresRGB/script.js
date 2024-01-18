62bffc1e5e3289f3a9a4e6ba6b06b16c695e1fec
Sherklan12e <davismendoza.08@gmail.com>
2024-01-18 20:12:14 -0300

    changes

--- Proyectos/Sheklan12e/ColoresRGB/script.js
+++ Proyectos/Sheklan12e/ColoresRGB/script.js
@@ -1,1 +1,21 @@
-con
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+
+
+function generarColor(){
+  let digitos = '0123456789ABCDEF';
+  let colorHex = '#'
+  for (let i = 0; i < 6; i++){
+    let indiceAleatorio = Math.floor(Math.random() * 16)
+    colorHex  += digitos[indiceAleatorio]
+
+  }
+
+  return colorHex
+}
+
+boton.addEventListener('click', function(){
+  let colorAreatorio = generarColor();
+  color.textContent = colorAreatorio;
+  document.body.style.backgroundColor = colorAreatorio
+})
