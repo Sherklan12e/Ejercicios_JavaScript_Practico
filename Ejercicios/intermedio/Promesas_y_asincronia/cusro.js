be8d4350d7d60d1d8efbd9197faa2da5e6a01687
Sherklan12e <davismendoza.08@gmail.com>
2024-01-20 14:03:14 -0300

    calls

---
+++ Ejercicios/intermedio/Promesas_y_asincronia/cusro.js
@@ -0,0 +1,5 @@
+const resta = function(a ,b){
+    return a- b;
+}
+console.log(resta(1,3)
+)
---
+++ Proyectos/Sheklan12e/DarkMode/script.js
@@ -0,0 +1,6 @@
+var Animate = {
+    animate : function(options){
+        let el = document.querySelector(options.selector);
+        
+    }
+}
