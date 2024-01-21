dd44cd4b7f2e545b52bb2bbe89c0fc288526231a
Sherklan12e <davismendoza.08@gmail.com>
2024-01-21 19:23:33 -0300

    AJAX

---
+++ holamundo/freecodecamp/principio.js
@@ -0,0 +1,12 @@
+let arreglo = [56,3,324,23,2]
+
+console.log(arreglo[3])
+arreglo.push('fs')
+console.log(arreglo)
+
+arreglo.pop()
+console.log(arreglo)
+arreglo.shift()
+console.log(arreglo)
+arreglo.unshift('Davis el patrixc')
+console.log(arreglo)
