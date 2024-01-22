39c23c3d649ec8969e3465e955465ab029cd55a5
Sherklan12e <davismendoza.08@gmail.com>
2024-01-22 12:40:06 -0300

    colores

--- Proyectos/Sheklan12e/ColoresRGB/script.js
+++ Proyectos/Sheklan12e/ColoresRGB/script.js
@@ -1,21 +1,18 @@
-const boton = document.querySelector('button');
-const color = document.getElementById('color');
+const boton = document.getElementById('cambiar-color')
 
 
-function generarColor(){
-  let digitos = '0123456789ABCDEF';
-  let colorHex = '#'
-  for (let i = 0; i < 6; i++){
-    let indiceAleatorio = Math.floor(Math.random() * 16)
-    colorHex  += digitos[indiceAleatorio]
+function generador(){
+  datos = '123456789ABCDEF'
+  palabra = '#'
+  for (let i =0; i<6; i++){
+    random = Math.floor(Math.random() * 10 );
+    palabra += datos[random]
 
   }
-
-  return colorHex
+  return palabra
 }
 
-boton.addEventListener('click', function(){
-  let colorAreatorio = generarColor();
-  color.textContent = colorAreatorio;
-  document.body.style.backgroundColor = colorAreatorio
+boton.addEventListener( 'click' , function(){
+  let colores = generador()
+  document.body.style.backgroundColor = colores
 })
