7c2fc0b7f6498c17ceaddce2c6b3d52b4fa2a13e
Sherklan12e <davismendoza.08@gmail.com>
2024-03-17 19:27:47 -0300

    let and const js

---
+++ Ejercicios/avanzado/ES6_promesas/1sherklan12e.js
@@ -0,0 +1,6 @@
+const Pi = 3; // valor fijo
+let num1 = 4; // puede cambiar
+num1 = 5
+console.log(num1)
+
+// La diferencia es que const solo se usa para valores constantes que no se pueden modificar al correr el codigo
